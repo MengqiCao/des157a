55545ea492bd0129887dff1ec8ee31612ac487a7
MengqiCao <aricao@ucdavis.edu>
2023-10-30 12:43:43 -0700

    message

--- studio1_medlibs/script.js
+++ studio1_medlibs/script.js
@@ -9,14 +9,15 @@
     
         const petName = document.querySelector('#petname').value;
         const adjective1 = document.querySelector('#adjective1').value;
-        const verbIng1 = document.querySelector('#verb_ing').value;
+        const verbIng1 = document.querySelector('#verb_ing1').value;
         const verbIng2 = document.querySelector('#verb_ing2').value;
         const color = document.querySelector('#color').value;
         const location = document.querySelector('#location').value;
         const verb1 = document.querySelector('#verb1').value;
         const ability = document.querySelector('#ability').value;
+        const noun = document.querySelector('#noun').value;
     
-        const story = `${petName} was a/an ${adjective1} creature with a body shaped like a [Noun]. Unlike other pixel pets, its favorite activities included ${verbIng1} and ${verbIng2}. Its ${color} hue shimmered whenever it ventured into the sunlight, casting playful reflections around. It spends its time in the ${location} the most to ${verb1}. Whenever it felt particularly happy or excited, it could ${ability}.`;
+        const story = `${petName} was a/an ${adjective1} creature with a body shaped like a ${noun}. Unlike other pixel pets, its favorite activities included ${verbIng1} and ${verbIng2}. Its ${color} hue shimmered whenever it ventured into the sunlight, casting playful reflections around. It spends its time in the ${location} the most to ${verb1}. Whenever it felt particularly happy or excited, it could ${ability}.`;
     
         document.getElementById('storyText').innerText = story;
         document.getElementById('overlay').classList.remove('hidden');
